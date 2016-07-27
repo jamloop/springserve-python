fe33f5ceaaa1aa74ed8c96401c137d0d3527517c
Dave <dave@yhmg.com>
2016-07-27 12:11:27 -0400

    fixing delete

--- springserve/__init__.py
+++ springserve/__init__.py
@@ -1,6 +1,6 @@
 
 #import all of this version information
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 __author__ = 'dave@springserve.com'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2016 Springserve'
