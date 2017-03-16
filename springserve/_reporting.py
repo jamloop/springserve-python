51878a03ab2d8d98291debbd70adb03d736a03ee
Daphne Paparis <dpaparis@springserve.com>
2017-03-16 11:17:18 -0400

    initialize dataframe attribute in get all pages fix

--- springserve/_reporting.py
+++ springserve/_reporting.py
@@ -49,6 +49,8 @@ class _ReportingResponse(_VDAPIMultiResponse):
         if clear_previous:
             self.dataframe = new_data
         else:
+            #call to dataframe to make sure self.dataframe is set
+            self.to_dataframe()
             self.dataframe = self.dataframe.append(new_data)
         self._current_page += 1
         return True
