5638c04f0123fd2240e3d414eb83397a79a789a2
dave <dave@dave.local>
2021-02-10 12:53:29 -0500

    bumping version

--- springserve/__init__.py
+++ springserve/__init__.py
@@ -8,7 +8,7 @@ if six.PY3:
     from builtins import object
 
 
-__version__ = '0.7.8' #TODO: This is duplicated in the build.  Need to figure how to set this once 
+__version__ = '0.7.9' #TODO: This is duplicated in the build.  Need to figure how to set this once 
 
 import sys as _sys
 import json as _json
